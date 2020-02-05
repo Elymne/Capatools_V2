8871640cc21951e30a9d42862b280d906bd914fc
viaud-j <viaud.julien@capacites.fr>
2020-02-05 16:40:54 +0100

    - Import des utilisateurs : ajout d'une commande console: "yii load-user -f=XXXX" pour le chargement automatique des utilisateurs.

---
+++ commands/LoadUserController.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * @link http://www.yiiframework.com/
+ * @copyright Copyright (c) 2008 Yii Software LLC
+ * @license http://www.yiiframework.com/license/
+ */
+
+namespace app\commands;
+
+use yii\console\Controller;
+use yii\console\ExitCode;
+
+
+use app\models\User\Capaidentity;
+use app\models\User\Cellule;
+
+
+/**
+ * This command echoes the first argument that you have entered.
+ *
+ * This command is provided as an example for you to learn how to create console commands.
+ *
+ * @author Qiang Xue <qiang.xue@gmail.com>
+ * @since 2.0
+ */
+class LoadUserController extends Controller
+{
+    
+    public $file ='';
+    public $NonAdm ='';
+    public $PrenonAdm ='';
+
+    public function options($actionID)
+    {
+        return ['file'];
+    }
+    public function optionAliases()
+    {
+        return ['f' => 'file'];
+    }
+
+    /**
+     * This command echoes what you have entered as the message.
+     * @param string $message the message to be echoed.
+     * @return int Exit code
+     */
+    public function actionIndex()
+    {
+        echo $this->file . "\n";
+        $bresult = ExitCode::OK;
+
+       if( ($readfile = fopen($this->file, "r"))!== FALSE)
+       {
+            //Lecture de l'entête
+            $data = fgetcsv($readfile,10000,';');
+
+            //Lecture des donnéess
+            while (($data = fgetcsv($readfile,10000,';')) !== FALSE)
+            {
+                $user = new Capaidentity();
+                //Username Nom Prenom
+                $user->username =  $data[1].' '.$data[0];
+                $user->email =  $data[2];
+
+                $user->Celluleid = Cellule::findByAXX($data[3])->id;
+                $user->generatePasswordAndmail();
+                $user->save();
+                
+            } 
+       }
+       else
+       {
+            $bresult = ExitCode::NOINPUT;
+       }
+
+
+
+
+        return ExitCode::OK;
+    }
+}
