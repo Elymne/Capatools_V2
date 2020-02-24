92828e93f7f211bc1cbb546f97b3ffbbcdb40315
sacha DJURDJEVIC <sacha.djurdjevic@gmail.com>
2020-02-24 16:13:14 +0100

    fix(migrate): change tablename devisstatus to devis_status.

--- migrations/m200213_125952_create_devis_statut_table.php
+++ migrations/m200213_125952_create_devis_statut_table.php
@@ -3,63 +3,62 @@
 use yii\db\Migration;
 
 /**
- * Handles the creation of table `{{%devisstatut}}`.
+ * Handles the creation of table `{{%devis_status}}`.
  */
-class m200213_125952_create_devisstatut_table extends Migration
+class m200213_125952_create_devis_statut_table extends Migration
 {
     /**
      * {@inheritdoc}
      */
     public function safeUp()
     {
-        $this->createTable('{{%devisstatut}}', [
+        $this->createTable('{{%devis_status}}', [
             'id' => $this->integer(),
-            'label' =>$this->string(),
+            'label' => $this->string(),
         ]);
 
-        $this->execute('ALTER TABLE devisstatut AUTO_INCREMENT = 0');
+        $this->execute('ALTER TABLE devis_status AUTO_INCREMENT = 0');
 
         //Ajout de la statut Avant Contrat
-        $this->insert('{{%devisstatut}}', [
+        $this->insert('{{%devis_status}}', [
             'id' => '0',
             'label' => 'Avant contrat'
         ]);
 
         //Ajout de la statut Projet en cours
-        $this->insert('{{%devisstatut}}', [
+        $this->insert('{{%devis_status}}', [
             'id' => '1',
             'label' => 'Projet en cours'
         ]);
 
         //Ajout de la statut Projet annulé
-        $this->insert('{{%devisstatut}}', [
+        $this->insert('{{%devis_status}}', [
             'id' => '2',
             'label' => 'Projet annulé'
         ]);
 
         //Ajout de la statut Projet terminé
-        $this->insert('{{%devisstatut}}', [
+        $this->insert('{{%devis_status}}', [
             'id' => '3',
             'label' => 'Projet terminé'
         ]);
 
         //Ajout de la statut Projet en attente validation resopnsable opérationel
-        $this->insert('{{%devisstatut}}', [
+        $this->insert('{{%devis_status}}', [
             'id' => '4',
             'label' => 'Attente validation Opérationel'
         ]);
 
         //Ajout de la statut Projet en attente validation resopnsable opérationel
-        $this->insert('{{%devisstatut}}', [
+        $this->insert('{{%devis_status}}', [
             'id' => '5',
             'label' => 'Attente validation client'
         ]);
-        
-        $this->execute('ALTER TABLE devisstatut ADD PRIMARY KEY (id)');
-        
+
+        $this->execute('ALTER TABLE devis_status ADD PRIMARY KEY (id)');
+
 
         $this->addColumn('devis', 'statut_id', $this->integer());
-
     }
 
     /**
@@ -67,8 +66,8 @@ class m200213_125952_create_devisstatut_table extends Migration
      */
     public function safeDown()
     {
-        $this->dropTable('{{%devisstatut}}');
-        
+        $this->dropTable('{{%devis_status}}');
+
         $this->dropColumn('devis', 'statut_id', $this->integer());
     }
 }
