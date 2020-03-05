8c2d34ca78cd518b3fbf3a0e6d5debcfbaf4dd24
sacha DJURDJEVIC <sacha.djurdjevic@gmail.com>
2020-03-05 14:11:51 +0100

    refactor(ux-ui): admin index view.

--- views/administration/index.php
+++ views/administration/index.php
@@ -1,5 +1,6 @@
 <?php
 
+use app\widgets\TopTitle;
 use yii\helpers\Html;
 use yii\grid\GridView;
 use yii\widgets\Pjax;
@@ -10,96 +11,97 @@ use yii\widgets\Pjax;
 $this->title = 'Liste des salariés';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="capa_user-index">
 
-    <div class="row">
-        <div class="card">
+<?= TopTitle::widget(['title' => $this->title]) ?>
 
-            <div class="card-content">
-                <span class="card-title"><?= Html::encode($this->title) ?></span>
-            </div>
+<div class="container">
+    <div class="capa_user-index">
 
-            <div class="card-action">
+        <div class="row">
+            <div class="card">
 
-                <?php Pjax::begin(); ?>
+                <div class="card-content">
 
-                <?= GridView::widget([
-                    'dataProvider' => $dataProvider,
-                    'filterModel' => $searchModel,
-                    'tableOptions' => [
-                        'class' => ['highlight']
-                    ],
-                    'columns' => [
-                        [
-                            'attribute' => 'username',
-                            'format' => 'raw',
-                            'label' => 'Salarié',
-                            'filterInputOptions' => [
-                                'class' => 'form-control',
-                                'placeholder' => 'filtre salarié'
-                            ],
-                            'value' => function ($data) {
-                                return Html::a($data['username'], ['administration/view', 'id' => $data['id']]);
-                            }
+                    <?php Pjax::begin(); ?>
+
+                    <?= GridView::widget([
+                        'dataProvider' => $dataProvider,
+                        'filterModel' => $searchModel,
+                        'tableOptions' => [
+                            'class' => ['highlight']
                         ],
-                        [
-                            'label' => 'Email',
-                            'format' => 'ntext',
-                            'attribute' => 'email',
-                            'filterInputOptions' => [
-                                'class' => 'form-control',
-                                'placeholder' => 'Filtre Email'
-                            ]
-
-                        ],
-                        [
-                            'label' => 'Cellule',
-                            'format' => 'ntext',
-                            'attribute' => 'cellule.name',
-                            'filterInputOptions' => [
-                                'class' => 'form-control',
-                                'placeholder' => 'Filtre Cellule'
-                            ]
-
-                        ],
-                        [
-                            'class' => 'yii\grid\ActionColumn',
-                            'template' => '{update} {delete} {link}',
-                            'header' => 'Actions',
-                            'buttons' => [
-                                'update' => function ($url, $model, $key) {
-                                    $options = [
-                                        'class' => 'btn-floating orange tooltipped',
-                                        'data-position' => 'bottom',
-                                        'data-tooltip' => 'Mettre à jour'
-                                    ];
-                                    return Html::a('<i class="material-icons">mode_edit</i>', $url, $options);
-                                },
-                                'delete' => function ($url, $model, $key) {
-                                    $options = [
-                                        'class' => 'btn-floating red tooltipped',
-                                        'data-position' => 'bottom',
-                                        'data-tooltip' => 'Supprimer'
-                                    ];
-                                    return Html::a('<i class="material-icons">delete</i>', $url, $options);
-                                },
-                                'link' => function ($url, $model, $key) {
-                                    $options = [
-                                        'class' => 'btn-floating green tooltipped',
-                                        'data-position' => 'bottom',
-                                        'data-tooltip' => 'Réinitialiser'
-                                    ];
-                                    return Html::a('<i class="material-icons">sync</i>', $url, $options);
+                        'columns' => [
+                            [
+                                'attribute' => 'username',
+                                'format' => 'raw',
+                                'label' => 'Salarié',
+                                'filterInputOptions' => [
+                                    'class' => 'form-control',
+                                    'placeholder' => 'filtre salarié'
+                                ],
+                                'value' => function ($data) {
+                                    return Html::a($data['username'], ['administration/view', 'id' => $data['id']]);
                                 }
                             ],
-                        ]
-                    ],
-                ]); ?>
+                            [
+                                'label' => 'Email',
+                                'format' => 'ntext',
+                                'attribute' => 'email',
+                                'filterInputOptions' => [
+                                    'class' => 'form-control',
+                                    'placeholder' => 'Filtre Email'
+                                ]
 
-                <?php Pjax::end(); ?>
+                            ],
+                            [
+                                'label' => 'Cellule',
+                                'format' => 'ntext',
+                                'attribute' => 'cellule.name',
+                                'filterInputOptions' => [
+                                    'class' => 'form-control',
+                                    'placeholder' => 'Filtre Cellule'
+                                ]
 
+                            ],
+                            [
+                                'class' => 'yii\grid\ActionColumn',
+                                'template' => '{update} {delete} {link}',
+                                'header' => 'Actions',
+                                'buttons' => [
+                                    'update' => function ($url, $model, $key) {
+                                        $options = [
+                                            'class' => 'btn-floating orange tooltipped',
+                                            'data-position' => 'bottom',
+                                            'data-tooltip' => 'Mettre à jour'
+                                        ];
+                                        return Html::a('<i class="material-icons">mode_edit</i>', $url, $options);
+                                    },
+                                    'delete' => function ($url, $model, $key) {
+                                        $options = [
+                                            'class' => 'btn-floating red tooltipped',
+                                            'data-position' => 'bottom',
+                                            'data-tooltip' => 'Supprimer'
+                                        ];
+                                        return Html::a('<i class="material-icons">delete</i>', $url, $options);
+                                    },
+                                    'link' => function ($url, $model, $key) {
+                                        $options = [
+                                            'class' => 'btn-floating green tooltipped',
+                                            'data-position' => 'bottom',
+                                            'data-tooltip' => 'Réinitialiser'
+                                        ];
+                                        return Html::a('<i class="material-icons">sync</i>', $url, $options);
+                                    }
+                                ],
+                            ]
+                        ],
+                    ]); ?>
+
+                    <?php Pjax::end(); ?>
+
+                </div>
             </div>
         </div>
-    </div>
 
+    </div>
 </div>
