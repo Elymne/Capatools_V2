f9ca17719cda357098f593743b6dc160de65df02
sacha DJURDJEVIC <sacha.djurdjevic@gmail.com>
2020-03-05 14:20:57 +0100

    refactor(ux-ui): view admin in progress.

--- views/administration/view.php
+++ views/administration/view.php
@@ -1,5 +1,6 @@
 <?php
 
+use app\widgets\TopTitle;
 use yii\helpers\Html;
 use yii\widgets\DetailView;
 use yii\grid\GridView;
@@ -11,53 +12,68 @@ $this->title = "Détail du salarié: " . $model->username;
 $this->params['breadcrumbs'][] = ['label' => 'Capaidentities', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 \yii\web\YiiAsset::register($this);
+
 ?>
-<div class="capa_user-view">
 
-    <h1><?= Html::encode($this->title) ?></h1>
+<?= TopTitle::widget(['title' => $this->title]) ?>
 
-    <p>
-        <?= Html::a('Modifier <i class="material-icons right">mode_edit</i>', ['update', 'id' => $model->id], ['class' => 'btn waves-effect waves-light']) ?>
-        <?= Html::a('Supprimer <i class="material-icons right">delete</i> ', ['delete', 'id' => $model->id], [
-            'class' => 'btn waves-effect waves-light',
-            'data' => [
-                'confirm' => 'Etes vous sûr de vouloir supprimer ce salarié ?',
-                'method' => 'post',
-            ],
-        ]) ?>
-    </p>
+<div class="container">
+    <div class="capa_user-view">
 
-    <?= DetailView::widget([
-        'model' => $model,
-        'attributes' => [
-            [                      // the owner name of the model
-                'label' => 'Nom et prénom',
-                'attribute' => 'username',
-            ],
-            'email:email',
-            [                      // the owner name of the model
-                'label' => 'Nom de la cellule',
-                'attribute' => 'cellule.name',
-            ],
-        ],
-    ]) ?>
+        <div class="row">
+            <div class="card">
 
-    <?=
+                <div class="card-content">
+                    <p>
+                        <?= Html::a('Modifier <i class="material-icons right">mode_edit</i>', ['update', 'id' => $model->id], ['class' => 'btn waves-effect waves-light']) ?>
+                        <?= Html::a('Supprimer <i class="material-icons right">delete</i> ', ['delete', 'id' => $model->id], [
+                            'class' => 'btn waves-effect waves-light',
+                            'data' => [
+                                'confirm' => 'Etes vous sûr de vouloir supprimer ce salarié ?',
+                                'method' => 'post',
+                            ],
+                        ]) ?>
+                    </p>
+                </div>
 
-        GridView::widget([
-            'dataProvider' => $rightProvider,
-            'columns' => [
-                [
-                    'label' => 'Service',
-                    'attribute' => 'service',
-                ],
-                [
+                <div class="card-content">
+                    <?= DetailView::widget([
+                        'model' => $model,
+                        'attributes' => [
+                            [                      // the owner name of the model
+                                'label' => 'Nom et prénom',
+                                'attribute' => 'username',
+                            ],
+                            'email:email',
+                            [                      // the owner name of the model
+                                'label' => 'Nom de la cellule',
+                                'attribute' => 'cellule.name',
+                            ],
+                        ],
+                    ]) ?>
 
-                    'label' => 'Statut',
-                    'attribute' => 'role',
-                ],
-            ]
-        ]);
-    ?>
+                    <?=
 
+                        GridView::widget([
+                            'dataProvider' => $rightProvider,
+                            'columns' => [
+                                [
+                                    'label' => 'Service',
+                                    'attribute' => 'service',
+                                ],
+                                [
+
+                                    'label' => 'Statut',
+                                    'attribute' => 'role',
+                                ],
+                            ]
+                        ]);
+                    ?>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+</div>
 </div>
