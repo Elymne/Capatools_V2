831858bc0eb39e35990b878b0d7c90bf20bdcc56
sacha DJURDJEVIC <sacha.djurdjevic@gmail.com>
2020-05-19 11:04:13 +0200

    feat(components): add docs to components.

--- components/DiscoverService.php
+++ components/DiscoverService.php
@@ -5,27 +5,31 @@ namespace app\components;
 use yii\helpers\FileHelper;
 use Yii;
 use yii\base\Component;
-use yii\base\InvalidConfigException;
 
+/**
+ * Classe qui s'utilisera comme un singleton dans l'application.
+ * Elle s'instancie dans le fichier config/web.php
+ * L'utilité de cette classe est de proposer une fonction qui permet de récupérer le nom des contrôleurs présent dans l'application.
+ * 
+ * @version Capatools v2.0
+ * @since Classe existante depuis la Release v2.0
+ */
 class discoverService extends Component
 {
 
 
     /**
-     * Récupère la liste des services sauf le dashboard
+     * On utilise cette fonction pour retourner tous les contrôleurs sauf le dashboardController.
+     * Elle est utilisé car pour permettre de créer le menu de gauche qui se créer à partir des contrôleurs.
+     * 
+     * @return array
      */
     public function getServices()
     {
-
-
         $path = Yii::$app->getControllerPath();
-
         $data = array();
-
-
         $files = FileHelper::findFiles($path, array("fileTypes" => array("php")));
 
-        //Parcours du répeertoire contenant les controllers
         foreach ($files as $file) {
             include_once $file;
 
@@ -35,7 +39,6 @@ class discoverService extends Component
 
                 $class_name = $controllers[] = substr($filename, 0, $pos);
 
-                //On exclu le dashboard
                 if ($class_name != 'Dashboard') {
                     $class_name = 'app\controllers\\' . $class_name . 'Controller';
                     array_unshift($data, $class_name);
