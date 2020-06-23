21cb13d53b8f2186d2e50f26eda66a205d05c027
sacha DJURDJEVIC <sacha.djurdjevic@gmail.com>
2020-06-23 13:37:06 +0200

    feat(models): fix laboratory.

--- migrations/m200099_000840_equipment_repayment_table.php
+++ migrations/m200099_000840_equipment_repayment_table.php
@@ -2,48 +2,56 @@
 
 use yii\db\Migration;
 
-class m200099_000840_equipment_payment_table extends Migration
+class m200099_000840_equipment_repayment_table extends Migration
 {
 
     public function safeUp()
     {
 
-        $this->createTable('{{%equipment_payment}}', [
+        $this->createTable('{{%equipment_repayment}}', [
             'id' => $this->primaryKey(),
             'nb_days' => $this->string()->notNull(),
-            'nb_hour' => $this->integer()->defaultValue(0),
+            'nb_hours' => $this->integer()->defaultValue(0),
             'risk' => $this->string()->notNull(),
             'risk_days' => $this->integer()->defaultValue(0),
 
             // Foreign key.
             'equipment_id' => $this->integer()->notNull(),
-            'payment_id' => $this->integer()->notNull(),
+            'repayment_id' => $this->integer()->notNull(),
         ]);
 
         $this->addForeignKey(
-            'FK_equipment_payment-equipment',
-            '{{%equipment_payment}}',
+            'FK_equipment_repayment-equipment',
+            '{{%equipment_repayment}}',
             'equipment_id',
             '{{%equipment}}',
             'id'
         );
 
         $this->addForeignKey(
-            'FK_equipment_payment-payment',
-            '{{%equipment_payment}}',
-            'payment_id',
-            '{{%payment}}',
+            'FK_equipment_repayment-repayment',
+            '{{%equipment_repayment}}',
+            'repayment_id',
+            '{{%repayment}}',
             'id'
         );
 
-        $this->insert('{{%equipment_payment}}', [
-            'lot_number' => 1,
-            'nb_days' => $this->integer()->defaultValue(0),
-            'nb_hours' => $this->integer()->defaultValue(0),
-            'risk' => 'Normal',
+        $this->insert('{{%equipment_repayment}}', [
+            'nb_days' => 20,
+            'nb_hours' => 19,
+            'risk' => 'Haute',
             'risk_days' => 15,
             'equipment_id' => 1,
-            'payment_id' => 1
+            'repayment_id' => 1
+        ]);
+
+        $this->insert('{{%equipment_repayment}}', [
+            'nb_days' => 2000,
+            'nb_hours' => 1,
+            'risk' => 'Très haute',
+            'risk_days' => 239,
+            'equipment_id' => 2,
+            'repayment_id' => 2
         ]);
     }
 
