ff262e13506f70939f65da14e66c626bec1c9d58
viaud-j <viaud.julien@capacites.fr>
2020-10-02 09:56:33 +0200

    fix profil menu

--- widgets/TopMenuBar.php
+++ widgets/TopMenuBar.php
@@ -67,10 +67,10 @@ class TopMenuBar extends \yii\bootstrap\Widget
                         </ul>-->
                         <!-- profile-dropdown-->
                         <ul class="dropdown-content" id="profile-dropdown">
-                            <li><a class="grey-text text-darken-1" href="#"><i class="material-icons">person_outline</i>Profile</a></li>
+                          <!--  <li><a class="grey-text text-darken-1" href="#"><i class="material-icons">person_outline</i>Profile</a></li>
                             <li><a class="grey-text text-darken-1" href="page-faq.html"><i class="material-icons">help_outline</i>Help</a></li>
-                            <li class="divider"></li>
-                            <li><a class="grey-text text-darken-1" href="${logout}"><i class="material-icons">keyboard_tab</i>Logout</a></li>
+                            <li class="divider"></li>-->
+                            <li><a class="grey-text text-darken-1" href="${logout}"><i class="material-icons">keyboard_tab</i>Déconnection</a></li>
                         </ul>
                     </div>
                 </nav>
